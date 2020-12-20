3e15c656a130f6f9329b772b8a67dfd3f0749822
Corey <Corey255A1@hotmail.com>
2020-12-19 22:29:25 -0500

    Procedural Pipe Track.

--- src/3DPipeGameMain.ts
+++ src/3DPipeGameMain.ts
@@ -4,17 +4,22 @@ import "@babylonjs/loaders/glTF";
 import {AdvancedDynamicTexture, Button} from "@babylonjs/gui";
 import {  Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder, SceneLoader, FollowCamera, Material, StandardMaterial, Color3, Matrix, Axis, CSG, Texture, CubeTexture, FreeCamera } from "@babylonjs/core";
 
+
+const NINETYDEG = Math.PI/2;
+const FOURTYFIVEDEV = Math.PI/4;
+
 class PipeTree
 {
     public branches:Array<PipeTree>;
     public point:Vector3;
     public previous:PipeTree;
     public last_direction:Vector3;
+    public traversed:boolean;
     //public next_direction:Vector3;
     public static TurnSections:number = 64;
     public static TurnStep:number = Math.PI*2/PipeTree.TurnSections;
     constructor(previous:PipeTree, point:Vector3){
-
+        this.traversed =false;
         this.branches = [];
         this.previous = previous;
         this.point = point;
@@ -89,16 +94,21 @@ class PipeTree
     public Turn(angle:number, radius:number, direction:number):PipeTree
     {
         const section_size =(Math.PI*radius*2)/PipeTree.TurnSections;
+        //console.log(PipeTree.TurnStep)
         const startVector = this.last_direction.scale(section_size);
-        console.log(startVector);
+        //console.log(startVector);
         var rotMatrix = Matrix.RotationAxis(Axis.Y, PipeTree.TurnStep*direction);
         var previousPipe:PipeTree = this;
-        for(var theta=0; theta<angle-PipeTree.TurnStep; theta+=PipeTree.TurnStep)
+        var end  = (angle)-(PipeTree.TurnStep/2);
+        let i=0;
+        for(var theta=0; theta<end; theta+=PipeTree.TurnStep)
         {
             var newPipe:PipeTree = new PipeTree(previousPipe, previousPipe.point.add(Vector3.TransformNormal(previousPipe.last_direction, rotMatrix).scale(section_size)))
             previousPipe.branches.push(newPipe);
             previousPipe = newPipe;
+            i++;
         }
+        //console.log(theta);
 
         return previousPipe;
     }
@@ -117,10 +127,20 @@ class PipeTree
 
     }
 
+    public static JoinPipes(pipes:Array<PipeTree>, pointToJoin:Vector3, direction:Vector3):PipeTree{
+        let pipe:PipeTree = new PipeTree(pipes[0], pointToJoin)
+        let pipeEnd:PipeTree = pipe.PipeTo(pointToJoin.add(direction));
+        for(let p=0; p<pipes.length; p++){
+            pipes[p].AddExisting(pipe);
+        }
+        return pipeEnd;
+    }
+
 
     public static GeneratePipeHelper(root:PipeTree, meshes:Array<Mesh>, scene:Scene, leftPoints:Array<Vector3>, rightPoints:Array<Vector3>)
     {
         //if(tree.branches.length <= 1){
+            
             var points = root.GetTrackPoints();
             leftPoints.push(points[0]);
             rightPoints.push(points[1]);
@@ -134,14 +154,24 @@ class PipeTree
             //rightPoints = [];
         }
 
-        for(var tree of root.branches)
-        {
+       
+        for(var branch of root.branches)
+        {                
             if (root.branches.length !== 1)
             {
                 leftPoints = [points[0]];
                 rightPoints = [points[1]];
             }
-            PipeTree.GeneratePipeHelper(tree, meshes, scene, leftPoints, rightPoints);
+            if(branch.traversed === false){
+                PipeTree.GeneratePipeHelper(branch, meshes, scene, leftPoints, rightPoints);
+                branch.traversed = true;
+            }else{
+                var branchpoints = branch.GetTrackPoints();
+                leftPoints.push(branchpoints[0]);
+                rightPoints.push(branchpoints[1]);
+                meshes.push(MeshBuilder.CreateTube("left",{path:leftPoints, radius:0.2}, scene));
+                meshes.push(MeshBuilder.CreateTube("right",{path:rightPoints, radius:0.2}, scene));
+            }
         }
     }
 
@@ -189,6 +219,112 @@ class PipeTree
     }
 }
 
+class Point3D{
+    public x:number;
+    public y:number;
+    public z:number;
+    constructor(x,y,z){
+        this.x=x;
+        this.y=y;
+        this.z=z;
+    }
+}
+
+class MapNavi{
+    public point:Point3D;
+    public startingPoint:Point3D;
+    public h:number;
+    public width:number;
+    public height:number;
+    public depth:number;
+    public map:Array<Array<Array<number>>>;
+    public moves:Array<number>;
+    constructor(width,height,x,y,z){
+        this.point = new Point3D(x,y,z);
+        this.startingPoint = this.point;
+        this.width = width;
+        this.height = height;
+        this.depth = 4;
+        this.h=0;
+        this.map = [];
+        this.moves = [];
+        for(let x=0;x<this.width;x++){
+            this.map.push([]);
+            for(let y=0;y<this.height;y++){
+                this.map[x].push([]);
+                for(let z=0;z<this.depth;z++){
+                    this.map[x][y].push(-10);
+                }
+            }
+        }
+    }
+
+    MoveToStart(){
+        this.point = this.startingPoint;
+    }
+
+    Move(dir:number){
+        this.map[this.point.x][this.point.y][this.point.z] = dir;
+        this.point = this.GetPointInDir(dir);
+        if(dir!=5 && dir!=6)
+        {
+            this.h = this.GetWorldDirection(dir);
+        }
+        this.moves.push(dir);
+    }
+
+    GetMove():number{
+        return this.map[this.point.x][this.point.y][this.point.z];
+    }
+
+    NextMove(){
+        let newh = this.GetMove();
+        this.point = this.GetPointInDir(newh);
+        if(newh!=5 && newh!=6)
+        {
+            this.h = this.GetWorldDirection(newh);
+        }
+        console.log(this.point);
+    }
+
+    CheckBound(x:number, y:number, z:number):boolean{
+        return (x<0 || y<0 || z<0 || x>=this.width || y>=this.height || z>=this.depth)
+    }
+    TestDirection(dir:number):boolean{
+        let point = this.GetPointInDir(dir);
+        return !this.CheckBound(point.x, point.y, point.z) && this.map[point.x][point.y][point.z]===-10;
+    }
+    GetWorldDirection(dir:number){
+        let newh = this.h+dir; if(newh<0){newh+=4};
+        if(newh==4)newh=0;
+        return newh;
+    }
+    ChangeZ(up:boolean):Point3D{
+        if(up){
+           return new Point3D(this.point.x, this.point.y, this.point.z+1);           
+        }
+        else{
+            return new Point3D(this.point.x, this.point.y, this.point.z-1);
+        }
+    }
+    GetPointInDir(dir:number):Point3D{
+        if(dir==5 || dir==6){
+            return this.ChangeZ(dir==5);
+        }else{
+            let newh = this.GetWorldDirection(dir);
+            switch(newh){
+                case 1: return new Point3D(this.point.x+1, this.point.y, this.point.z);
+                case 2: return new Point3D(this.point.x, this.point.y-1, this.point.z);
+                case 3: return new Point3D(this.point.x-1, this.point.y, this.point.z);
+                case 0:
+                default:
+                return  new Point3D(this.point.x, this.point.y+1, this.point.z);
+            }
+        }
+
+    }
+}
+
 
 class Environment
 {
@@ -205,14 +341,14 @@ class Environment
         groundMat.diffuseColor = new Color3(0,1,0);
         this.ground.material = groundMat;
 
-        this.skybox = MeshBuilder.CreateBox("skyBox", {size:2000}, scene);
-        const skyboxMaterial = new StandardMaterial("skyBox", scene);
-        skyboxMaterial.backFaceCulling = false;
-        skyboxMaterial.reflectionTexture = new CubeTexture("imgs/skybox", scene);
-        skyboxMaterial.reflectionTexture.coordinatesMode = Texture.SKYBOX_MODE;
-        skyboxMaterial.diffuseColor = new Color3(0, 0, 0);
-        skyboxMaterial.specularColor = new Color3(0, 0, 0);
-        this.skybox.material = skyboxMaterial;
+        // this.skybox = MeshBuilder.CreateBox("skyBox", {size:2000}, scene);
+        // const skyboxMaterial = new StandardMaterial("skyBox", scene);
+        // skyboxMaterial.backFaceCulling = false;
+        // skyboxMaterial.reflectionTexture = new CubeTexture("imgs/skybox", scene);
+        // skyboxMaterial.reflectionTexture.coordinatesMode = Texture.SKYBOX_MODE;
+        // skyboxMaterial.diffuseColor = new Color3(0, 0, 0);
+        // skyboxMaterial.specularColor = new Color3(0, 0, 0);
+        // this.skybox.material = skyboxMaterial;
 
 
 
@@ -222,51 +358,175 @@ class Environment
         this.pipeTree = new PipeTree(undefined, new Vector3(0,0,0))
         var treebuilder = new PipeTree(this.pipeTree, new Vector3(0,0,2));
         this.pipeTree.branches.push(treebuilder);
+
+
+        const direction_randomizer_params = {
+            "0":[
+                {move:0,percent:0.7},
+                {move:1,percent:0.15},
+                {move:-1,percent:0.15}
+            ],
+            "1":[
+                {move:0,percent:0.25},
+                {move:1,percent:0.05},
+                {move:-1,percent:0.7}
+            ],
+            "2":[
+                {move:0,percent:0.0},
+                {move:1,percent:0.5},
+                {move:-1,percent:0.5}
+            ],
+            "3":[
+                {move:0,percent:0.25},
+                {move:1,percent:0.7},
+                {move:-1,percent:0.05}
+            ]        
+        }
         
-        //treebuilder = PipeTree.GenerateTrack(treebuilder, 20);
-        treebuilder = treebuilder.Straight(10,0);
+        
+        const max_movements = 10;
+        var Meshes:Array<Mesh> = []
+        for(let landmark=0;landmark<5;landmark++)
+        {
+            //Generate Intermediate Track
+            let navi = new MapNavi(16,64,8,0,0);
+            for(let p=0;p<max_movements;p++){
+                let ranmove = Math.random();
+                let params = direction_randomizer_params[navi.h];
+                let percentage = 0
+                for(let m=0;m<params.length;m++)
+                {
+                    percentage +=params[m].percent;
+                    if(percentage > 2){
+                        p=max_movements;
+                        break;
+                    }
+                    else if(ranmove < percentage){
+                        if(navi.TestDirection(params[m].move)){
+                            if(params[m].move == 0 && Math.random()>0.5){
+                                let zran = Math.random();
+                                if(zran > navi.point.z*0.1)
+                                {
+                                    if(navi.TestDirection(5)){
+                                        navi.Move(5);
+                                        break;
+                                    }
+                                    else if(navi.TestDirection(6)){
+                                        navi.Move(6);
+                                        break;
+                                    }
+                                }else{
+                                    if(navi.TestDirection(6)){
+                                        navi.Move(6);
+                                        break;
+                                    }
+                                    else if(navi.TestDirection(5)){
+                                        navi.Move(5);
+                                        break;
+                                    }
+                                }
+                            }
+                            navi.Move(params[m].move);
+                            break;
+                        }
+                    }
+                }                
+                //console.log(p);
+            }
+            switch(navi.h){
+                case 1:navi.Move(-1); break;
+                case 3: navi.Move(1);break;
+                case 2:
+                    if(navi.TestDirection(1)) navi.Move(1);
+                    else if(navi.TestDirection(-1)) navi.Move(-1);
+            }
+            navi.Move(0)
+
+            //Mark            
+            navi.moves.forEach((move)=>{
+                switch(move){
+                    case 0:treebuilder = treebuilder.Straight(10,0); break;
+                    case 1: treebuilder = treebuilder.Turn(NINETYDEG,10,1); break;
+                    case -1: treebuilder = treebuilder.Turn(NINETYDEG,10,-1); break;
+                    case 5: treebuilder = treebuilder.Straight(5,0.5);treebuilder = treebuilder.Straight(1,-0.5); break;
+                    case 6: treebuilder = treebuilder.Straight(5,-0.5);treebuilder = treebuilder.Straight(1,0.5); break;
+                }
+            })
+            var branches:Array<PipeTree>;
+            [branches] = this.CreateTunnel(treebuilder, scene,100);
+            treebuilder = PipeTree.JoinPipes(branches,treebuilder.GetPointStraight(200,0), treebuilder.last_direction);
+
+            //Meshes.push(Mesh.MergeMeshes(meshes, true));
+        }
+        var meshes:Array<Mesh> = [];
+        PipeTree.GeneratePipeMeshes(this.pipeTree,meshes,scene);
+        console.log("meshes " + meshes.length);
+       // Meshes.forEach(m=>m.isVisible = false);
+
+        //let endPoint = navi.point;
+        //console.log(navi.map);
 
 
-        // pipetree = pipetree.Straight(15,0.5);
+        
+
+        
+        //treebuilder = PipeTree.GenerateTrack(treebuilder, 40);
+        // treebuilder = treebuilder.Straight(10,0);
+        // var branches:Array<PipeTree>;
+        // treebuilder = PipeTree.GenerateTrack(treebuilder, 40);
+        // [branches] = this.CreateTunnel(treebuilder, scene,100);
+        // treebuilder = PipeTree.JoinPipes(branches,treebuilder.GetPointStraight(200,0));
+        // treebuilder = PipeTree.GenerateTrack(treebuilder, 40);
+
+        // [branches] = this.CreateTunnel(treebuilder, scene,100);
+        // treebuilder = PipeTree.JoinPipes(branches,treebuilder.GetPointStraight(200,0));
+        
+
+        //treebuilder= treebuilder.Straight(15,0);
         // pipetree = pipetree.Straight(5,0.5);
         // pipetree = pipetree.Straight(5,0.5);
-        // pipetree = pipetree.Turn(Math.PI/2, 5, 1);
+        // treebuilder = treebuilder.Turn(Math.PI*2, 5, 1);
+        // treebuilder= treebuilder.Straight(15,0);
+        // treebuilder = treebuilder.Turn(Math.PI*2, 5, -1);
+        // treebuilder= treebuilder.Straight(15,0);
+        // treebuilder = treebuilder.Turn(Math.PI, 5, 1);
+        // treebuilder= treebuilder.Straight(15,0);
+        // treebuilder = treebuilder.Turn(Math.PI, 5, 1);
+        // treebuilder= treebuilder.Straight(15,0);
         // pipetree = pipetree.Turn(Math.PI/2, 5, -1);
         // pipetree = pipetree.Straight(5,0);
         // pipetree = pipetree.Straight(5,0);
+       
 
-        var p1 = treebuilder.AddBranch(-Math.PI/4,20);
-        var p2 = treebuilder.AddBranch(-Math.PI/10,20);
-        var p3 = treebuilder.AddBranch(Math.PI/10,20);
-        var p4 = treebuilder.AddBranch(Math.PI/4,20); 
+        // var p1 = treebuilder.AddBranch(-Math.PI/4,20);
+        // var p2 = treebuilder.AddBranch(-Math.PI/8,20);
+        // var p3 = treebuilder.AddBranch(Math.PI/8,20);
+        // var p4 = treebuilder.AddBranch(Math.PI/4,20); 
         
-        p1 = p1.Turn(Math.PI/4,5,1);
-        p2 = p2.Turn(Math.PI/10,5,1);
-        p3 = p3.Turn(Math.PI/10,5,-1);
-        p4 = p4.Turn(Math.PI/4,5,-1);
+        // p1 = p1.Turn(Math.PI/4,5,1);
+        // p2 = p2.Turn(Math.PI/8,5,1);
+        // p3 = p3.Turn(Math.PI/8,5,-1);
+        // p4 = p4.Turn(Math.PI/4,5,-1);
 
-        var t1 = p1.point;
-        p1 = p1.Straight(20,0);
-        var t2 = p1.point;
-        var tunnel = Mesh.CreateBox("box", 20, scene)
-        tunnel.position.copyFrom(t1);
-        tunnel.position.z += 15;
-        var sourceMat = new StandardMaterial("sourceMat", scene);
-        sourceMat.wireframe = false;
-        sourceMat.backFaceCulling = false;
-    
-        var bhole = MeshBuilder.CreateTube("tunnel1",{path:[t1,t2], radius:4, sideOrientation:2}, scene);
-        bhole.material = sourceMat;
-        var tcut = CSG.FromMesh(bhole);
-        var tnnl = CSG.FromMesh(tunnel);
-        tunnel.dispose();
-        bhole.dispose();
 
-        var tb = tnnl.subtract(tcut).toMesh("csg",null,scene);
+        // var sourceMat = new StandardMaterial("sourceMat", scene);
+        // sourceMat.wireframe = false;
+        // sourceMat.backFaceCulling = false;
 
-        p2 = p2.Straight(20,0);
-        p3 = p3.Straight(20,0);
-        p4 = p4.Straight(20,0);
+        // var p21 = p2.point;
+        // var p22 = p2.GetPointStraight(20,0);
+        // var bhole2 = MeshBuilder.CreateTube("tunnel",{path:[p21,p22], radius:3, sideOrientation:2}, scene);
+        // //bhole2.material = sourceMat;
+        // var tcut2 = CSG.FromMesh(bhole2);
+        // bhole2.dispose();
+
+        // var tnnl = CSG.FromMesh(tunnel);
+        // tunnel.dispose();
+
+        // p1 = p1.Straight(20,0);
+        // p2 = p2.Straight(20,0);
+        // p3 = p3.Straight(20,0);
+        // p4 = p4.Straight(20,0);
 
         // p1 = PipeTree.GenerateTrack(p1, 20);
         // p2 = PipeTree.GenerateTrack(p2, 20);
@@ -327,10 +587,33 @@ class Environment
         // p4 = p4.Turn(Math.PI/2, 5, 1);
         // p4 = p4.Turn(Math.PI/2, 5, -1);
 
-        var meshes:Array<Mesh> = [];
-        PipeTree.GeneratePipeMeshes(this.pipeTree,meshes,scene);
-        
-        var pipeTrack:Mesh = Mesh.MergeMeshes(meshes, true);        
+      
+    }
+
+
+    CreateTunnel(pipetree:PipeTree, scene:Scene, tunnelLength:number):[Array<PipeTree>, Mesh]{
+        var t1 = pipetree.point;
+        var t2 = pipetree.GetPointStraight(tunnelLength/2+21,0);
+        let branches:Array<PipeTree> =[];
+        var tunnel = MeshBuilder.CreateBox("box", {width:tunnelLength*0.9, height:10, depth:tunnelLength*0.9}, scene)
+        tunnel.position.copyFrom(t2);
+        var tnnl = CSG.FromMesh(tunnel);
+        tunnel.dispose();
+        let angle = -Math.PI/4;
+        for(var b=0;b<4;b++){
+            let branch = pipetree.AddBranch(angle,20)
+            branch = branch.Turn(Math.abs(angle),5,angle<0?1:-1);
+            angle+=Math.PI/8;
+            if(Math.abs(angle)<0.001){angle=Math.PI/8;}
+            let tunnelEnd:Vector3 = branch.GetPointStraight(tunnelLength,0);
+            var holemesh = MeshBuilder.CreateTube("hole",{path:[branch.point,tunnelEnd], radius:3, sideOrientation:2}, scene);
+            var cutter = CSG.FromMesh(holemesh);
+            tnnl = tnnl.subtract(cutter)
+            holemesh.dispose();
+            branch = branch.Straight(tunnelLength,0);
+            branches.push(branch);
+        }
+        return [branches, tnnl.toMesh("decisionTunnel", undefined, scene)];
     }
 }
 
